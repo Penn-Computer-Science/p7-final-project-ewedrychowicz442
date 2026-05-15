7df69aae28aad2bcfb8904331783065b64e5f0c6
ewedrychowicz442 <ewedrychowicz442@phm.k12.in.us>
2026-05-15 09:47:07 -0400

    Update Pac-Man.py

--- Pac-Man.py
+++ Pac-Man.py
@@ -12,7 +12,6 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 color = 'blue'
 PI = math.pi
 direction = 0
-counter = 0
 
 def draw_board():
     num1 = ((HEIGHT - 50)//32) 
@@ -42,16 +41,22 @@ player_x = 450
 player_y = 663
 
 def draw_player():
-    player_image = (pygame.transform.scale(pygame.image.load(f'player_image.png'), (45, 45)))
+    player_image = (pygame.transform.scale(pygame.image.load(f'player_image.png'), (45, 45))) #load the player image and scale it to 45 by 45
     #RIGHT = 0, LEFT = 1, UP = 2, DOWN = 3
     if direction == 0:
-        screen.blit(player_image[counter // 5], (player_x, player_y))
+        screen.blit(player_image, (player_x, player_y)) #starting position
     elif direction == 1:
-        screen.blit(pygame. transform.flip(player_image[counter // 5], True, False), (player_x, player_y))
+        screen.blit(pygame.transform.flip(player_image, True, False), (player_x, player_y)) #flip pac man in the x direction
     elif direction == 2:
-        screen.blit(pygame.transform.rotate(player_image[counter // 5], 90), (player_x, player_y))
+        screen.blit(pygame.transform.rotate(player_image, 90), (player_x, player_y)) #rotate pac man 90 degrees counterclockwise
     elif direction == 3:
-        screen.blit(pygame.transform.rotate(player_image[counter // 5], 270), (player_x, player_y))
+        screen.blit(pygame.transform.rotate(player_image, 270), (player_x, player_y)) #rotate pac man 270 degrees counterclockwise
+
+def check_position(center_x, center_y):
+    turns = [False, False, False, False]
+    
+
+
 
 run = True
 while run:
@@ -59,10 +64,22 @@ while run:
     screen.fill('black')
     draw_board()
     draw_player()
+    center_x = player_x + 23
+    center_y = player_y + 24
+    check_position(center_x, center_y)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #if you exit out the game
             run = False
+        if event.type == pygame.KEYDOWN: #handles the moving of pac man
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
 
     pygame.display.flip() #draws it every iteration
 pygame.quit()
